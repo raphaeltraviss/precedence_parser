fa772c0963021a9bd2dc64b7a8bbf641fc12c612
Raphael Spencer <raphael@headway.io>
2019-04-04 14:58:38 -0500

    Actually test the parser

--- test.js
+++ test.js
@@ -4,6 +4,5 @@ const parse = require('./index');
 test('parser test', function (t) {
   t.plan(1);
   const result = parse("hey AND howdy IN foo AND bar IN header");
-  console.log("Parse output is...", result);
-  t.assert(true);
+  t.deepEqual(result, ['hey','howdy','foo','bar','header','IN','AND','IN','AND']);
 });
